27b3919a8fa56b1f3ebaec7a804949aae5a0e025
Brent Jackson <jxnblk@gmail.com>
2017-02-08 14:23:18 -0500

    Enable CORS

--- src/json.js
+++ src/json.js
@@ -8,6 +8,8 @@ module.exports = ({
 }) => {
   const [ h, s, l ] = chroma('#' + color).hsl()
   const pal = createPalette({ h, s, l })
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
 
   return JSON.stringify(pal, null, 2)
 }
